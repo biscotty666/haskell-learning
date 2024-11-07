8a86069118f02ddd3113b668fd7cd2d1dbd704e9
biscotty666 <biscotty666@gmail.com>
2024-11-06 22:04:01 -0700

    Haskell parsing JSON;

--- get-programming-with-Haskell/Unit-7/shell.nix
+++ get-programming-with-Haskell/Unit-7/shell.nix
@@ -1,11 +1,14 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs { config = {}; overlays = []; };
-  myHaskell = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [ stack ]);
+  myHaskell = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+    stack ]);
 in
 
 pkgs.mkShell {
   packages = with pkgs; [
     myHaskell
+    haskellPackages.http-conduit
+    sqlite
   ];
 }
