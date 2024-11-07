1520598645cd571d4bef2fb23f11390be916555b
biscotty666 <biscotty666@gmail.com>
2024-11-06 21:59:14 -0700

    Exercises from Real World Haskell;

---
+++ Real-World-Haskell/ch05/PrettyStub.hs
@@ -0,0 +1,22 @@
+module PrettyStub where
+
+import SimpleJSON
+
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+fsep :: [Doc] -> Doc
+fsep xs = undefined
